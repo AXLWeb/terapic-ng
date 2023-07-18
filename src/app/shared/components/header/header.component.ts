c2749e3f4614f395e2fc4b61217a8f12155e2624
Alex <alexvisualsound@gmail.com>
2023-07-19 00:27:17 +0200

    Add using routerLink to style navigation items

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { NavigationItem } from 'src/app/models';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  public activeLink: string = '';
 
+  public navigationItems: NavigationItem[] = [
+    { label: 'Terapic', link: '#intro' },
+    { label: 'Áreas de tratamiento', link: '#tratamiento' },
+    { label: 'Técnicas', link: '#tecnicas' },
+    { label: 'Centro', link: '#centro' },
+    { label: 'Contacto', link: '#contacto' },
+  ];
+
+  public setActiveLink(link: string) {
+    this.activeLink = link;
+  }
 }
